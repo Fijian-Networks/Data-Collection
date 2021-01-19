a59c1ec544f64035464d4149f1a747e09ef7f606
kerianVaraine <chambervulgarus@gmail.com>
2021-01-20 11:04:13 +1300

    loading from external storage DB every launch

--- ccSocial/lib/src/services/db_service.dart
+++ ccSocial/lib/src/services/db_service.dart
@@ -10,38 +10,24 @@ import 'package:ccSocial/src/models/person.dart';
 class DatabaseService {
   DatabaseService();
 
-  String _dbPath = "/storage/emulated/0/opendatakit/default/data/webDB/";
+  String _dbPath = "/storage/emulated/0/opendatakit/default/data/webDb/";
   Database _db;
-  //NAME OF DATABASE FILE IN ASSETS :: Temporary for developement. final production will be sqlite.db in android ODK-X folder
   String dbName = "sqlite.db";
 
   initDatabase() async {
-//    _db = await openDatabase('assets/' + db_name);
     _db = await openDatabase(_dbPath + dbName);
-
-    var databasePath = await getDatabasesPath();
-    var path = join(databasePath, dbName);
+    var path = join(_dbPath, dbName);
     print("dbPath in db_service :: " + path);
+    // TODO: catch errors if no db exists on storage
+    //check if parent directory exists
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (_) {}
 
-    //check if DB exists
-    var exists = await databaseExists(path);
+    var data = File(path).readAsBytesSync();
 
-    if (!exists) {
-      print('Creating a new DB copy from assets');
-
-      //check if parent directory exists
-      try {
-        await Directory(dirname(path)).create(recursive: true);
-      } catch (_) {}
-
-      //copy from assets
-      ByteData data = await rootBundle.load(join(_dbPath, dbName));
-      List<int> bytes =
-          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-
-      //write and flush bytes
-      await File(path).writeAsBytes(bytes, flush: true);
-    }
+    //write and flush bytes
+    await File(path).writeAsBytes(data, flush: true);
 
     //Open the DB
     _db = await openDatabase(path, readOnly: true);
