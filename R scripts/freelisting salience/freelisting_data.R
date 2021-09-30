fa07a32bfbacbefc4c41dcda8715e2837a748cf2
kerianVaraine <chambervulgarus@gmail.com>
2021-10-01 09:43:34 +1300

    moved all contents

--- R scripts/freelisting salience/freelisting_data.R
+++
@@ -1,8 +0,0 @@
-# testing on coded csv with bias
-tucsv <- tidyUnitCsv
-
-# Calc Salience
-FL <-CalculateSalience(tucsv)
-
-# DealWithDoubles handles double ups in single id's choices, in case they list the same thing twice
-FL.S <- SalienceByCode(FL, dealWithDoubles = "MAX")
--- ccSocial/android/app/src/main/kotlin/com/example/ccSocial/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ccSocial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
